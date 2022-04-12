ec14aa2fd72f6abdd98c398ccf223805607ba7a8
Oyewande Oluwaseun <seunemmanuel2255@gmail.com>
2022-04-12 21:12:41 +0100

    Connected App to Home page and addressShortener.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
+import Home from "./Pages/home";
+import getEthers from "./getEthers";
+// import { Contract, utils } from "ethers";
 
 function App() {
+  const [address, setAddress] = useState("Connect Wallet");
+  const [provider, setProvider] = useState({});
+
+  useEffect(() => {
+    const getData = async () => {
+      const provid = await getEthers();
+      setProvider(provid);
+    };
+
+    getData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const initialize = async () => {
+    const signer = provider.getSigner();
+    const address = await signer.getAddress();
+    setAddress(address);
+  };
+
   return (
     <Box flexGrow={1}>
-
+      <Home setAddress={setAddress} address={address} initialize={initialize} />
     </Box>
   );
 }
