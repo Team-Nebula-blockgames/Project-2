47b22d79d682b9739b6e5ca19ce6c8b6ab536387
Oyewande Oluwaseun <seunemmanuel2255@gmail.com>
2022-04-14 01:05:30 +0100

    MAdea card componnet for each file

---
+++ client/src/Components/card.js
@@ -0,0 +1,95 @@
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+function Card(props) {
+  const { name, description, access, setShareModal } = props;
+
+  return (
+    <Box
+      sx={{
+        width: "183px",
+        height: "160px",
+        background: "#444444",
+        borderRadius: "8px",
+        position: "relative",
+        "&:hover": {
+          backgroundColor: "#737373",
+        },
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "flex-start",
+          padding: "0px",
+          position: "absolute",
+          width: "84.1%",
+          height: "78%",
+          left: "14px",
+          top: "30px",
+        }}
+      >
+        <Typography
+          sx={{
+            fontWeight: 700,
+            fontSize: "20px",
+            lineHeight: "20px",
+            color: "#FFFFFF",
+          }}
+        >
+          {name}
+        </Typography>
+        <Typography
+          sx={{
+            fontWeight: 500,
+            fontSize: "12px",
+            lineHeight: "15px",
+            color: "#AAAAAA",
+          }}
+        >
+          {description}
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "flex-start",
+            padding: "0px",
+            position: "absolute",
+            width: "100%",
+            height: "15px",
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <Typography
+            sx={{
+              fontWeight: 700,
+              fontSize: "12px",
+              lineHeight: "15px",
+              color: access === "public" ? "#000000" : "#B973FF",
+            }}
+          >
+            {access}
+          </Typography>
+          <Typography
+            sx={{
+              fontWeight: 500,
+              fontSize: "12px",
+              lineHeight: "15px",
+              color: "#CCCCCC",
+              cursor: "pointer",
+            }}
+            onClick={() => setShareModal(true)}
+          >
+            Share
+          </Typography>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+export default Card;
